94e714fdfe82b3490898e965cbdfa10029b9dfd1
Leonardo Holanda <leonardo_holanda1999@hotmail.com>
2022-08-03 12:32:52 -0300

    Atualizações estéticas

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ function cadastrarDespesa() {
 
 		document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
 		document.getElementById('modal_titulo_div').className = 'modal-header text-success'
-		document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso!'
+		document.getElementById('modal_conteudo').innerHTML = 'Despesa cadastrada com sucesso!'
 		document.getElementById('modal_btn').innerHTML = 'Voltar'
 		document.getElementById('modal_btn').className = 'btn btn-success'
 
